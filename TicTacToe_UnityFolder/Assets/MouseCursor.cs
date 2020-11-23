d2326a6f74093ff67fbf8980fc5060a6e113c57f
KacperBazan <kbazan2782@bths.edu>
2020-11-23 08:46:07 -0500

    Added Unity folder  + some organization

---
+++ TicTacToe_UnityFolder/Assets/MouseCursor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    public GameObject hoverObject;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Space"))
+        {
+            hoverObject = collision.gameObject;
+        }
+        else
+        {
+            hoverObject = null;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        hoverObject = null;
+    }
+}
